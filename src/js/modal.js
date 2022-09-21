f9e18d7a6512b53c25d3c302f47fbe6201021360
Юлия Пшеничная <yulenykaa@gmail.com>
2022-09-21 04:36:56 +0300

    bg

--- src/js/modal.js
+++ src/js/modal.js
@@ -12,7 +12,8 @@
     refs.openModalBtnOffer.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
-    function toggleModal() {
+  function toggleModal() {
+      document.body.classList.toggle("modal-open");
       refs.modal.classList.toggle("is-hidden");
     }
   })();
